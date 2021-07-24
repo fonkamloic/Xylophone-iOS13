27a1f56a5b71370345db2be98a3028dd18b1cc85
Fonkam Loïc <fonkamloic@gmail.com>
2021-07-24 13:47:14 +0100

    working layout in storyboard

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    func playSound(sound: String) {
+            let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player?.play()
+                    
+        }
     
 
+    @IBAction func KeyPressed(_ sender: UIButton) {
+        print(sender.currentTitle?.description ?? "C");
+        playSound(sound: sender.currentTitle?.description ?? "C");
+
+    }
 }
+
+
 
